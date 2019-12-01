c370e9eb8141f2b07398b3a9c12badc9e53417c1
Justin Crawford <Justin@stacksmash.net>
2019-12-01 05:17:50 -0800

    Add this cute little function while I'm thinking about it

--- src/main/java/me/zacherycoleman/lolbans/IPBanning/IPBanUtil.java
+++ src/main/java/me/zacherycoleman/lolbans/IPBanning/IPBanUtil.java
@@ -5,12 +5,20 @@ import inet.ipaddr.IPAddress;
 import inet.ipaddr.IPAddressString;
 import java.net.InetAddress;
 import java.sql.*;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 import javax.sql.rowset.serial.SerialBlob;
 import me.zacherycoleman.lolbans.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+
+
+// Javadocs for IPAddress: https://seancfoley.github.io/IPAddress/IPAddress/apidocs/
 
 
 public class IPBanUtil 
@@ -32,14 +40,12 @@ public class IPBanUtil
 					if (!hn.isAddress())
 						return Optional.ofNullable(null);
 
-					IPAddress addr = hn.asAddress();
-
 					synchronized(Main.BannedAddresses)
 					{
 						for (IPAddressString cb : Main.BannedAddresses)
 						{
 							// They're a banned cidr, query for the reason and kick them.
-							if (cb.contains(addr.toAddressString()))
+							if (cb.contains(hn.asAddressString()))
 							{
 								PreparedStatement pst = self.connection.prepareStatement("SELECT * FROM IPBans WHERE IPAddress = ?");
 								pst.setString(1, cb.toString());
@@ -65,5 +71,26 @@ public class IPBanUtil
 		return t;
 	}
 
+	/**
+	 * Calculate the percentage of users that will be banned using
+	 * this CIDR. Useful for thresholds when a ban may be set but
+	 * it would be wildly overreaching and should be avoided.
+	 * @return
+	 */
+	public static float CalculateBanPercentage(IPAddressString address)
+	{
+		// Iterate the users and compare their addresses to the CIDR given to us
+		int TotalAffected = 0;
+		Collection<? extends Player> Players = Bukkit.getOnlinePlayers();
+		for (Player p : Players)
+		{
+			HostName hn = new HostName(p.getAddress());
 
+			if (address.contains(hn.asAddressString()))
+				TotalAffected++;
+		}
+
+		float percentage = (TotalAffected / Players.size()) * 100.0f;
+		return percentage;
+	}
 }
