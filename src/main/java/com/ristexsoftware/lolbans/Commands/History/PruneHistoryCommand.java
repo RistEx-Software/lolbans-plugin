aa07ac81bfae349634710682745871f8a1716a5d
Justin Crawford <Justin@stacksmash.net>
2020-05-08 09:30:06 -0700

    Fix the entirety of the Prune History command

--- src/main/java/com/ristexsoftware/lolbans/Commands/History/PruneHistoryCommand.java
+++ src/main/java/com/ristexsoftware/lolbans/Commands/History/PruneHistoryCommand.java
@@ -14,17 +14,20 @@ import com.ristexsoftware.lolbans.Utils.TimeUtil;
 import com.ristexsoftware.lolbans.Objects.Punishment;
 import com.ristexsoftware.lolbans.Objects.RistExCommandAsync;
 import com.ristexsoftware.lolbans.Objects.User;
+import com.ristexsoftware.lolbans.Utils.ArgumentUtil;
 import com.ristexsoftware.lolbans.Utils.DatabaseUtil;
 import com.ristexsoftware.lolbans.Utils.Messages;
 import com.ristexsoftware.lolbans.Utils.PermissionUtil;
 import com.ristexsoftware.lolbans.Utils.PunishID;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.regex.Pattern;
 import java.util.Optional;
 import java.util.TreeMap;
 import java.sql.*;
 
+// TODO: All the messages need to be run through Messages.yml
 public class PruneHistoryCommand extends RistExCommandAsync
 {
     private Main self = (Main) this.getPlugin();
@@ -36,6 +39,140 @@ public class PruneHistoryCommand extends RistExCommandAsync
         this.setPermission("lolbans.history.prune");
     }
 
+    private boolean HandlePunishmentID(CommandSender sender, String searchable, String[] args) throws SQLException, InterruptedException, ExecutionException
+    {
+        PreparedStatement RegexStatement = self.connection.prepareStatement("SELECT * FROM RegexBans WHERE PunishID = ?");
+        PreparedStatement IPBanStatement = self.connection.prepareStatement("SELECT * FROM IPBans WHERE PunishID = ?");
+        PreparedStatement Reports = self.connection.prepareStatement("SELECT * FROM Reports WHERE PunishID = ?");
+        
+        Future<Optional<ResultSet>> regexres = DatabaseUtil.ExecuteLater(RegexStatement);
+        Future<Optional<ResultSet>> ipbansres = DatabaseUtil.ExecuteLater(IPBanStatement);
+        Future<Optional<ResultSet>> reportsres = DatabaseUtil.ExecuteLater(Reports);
+        Optional<Punishment> punish = Punishment.FindPunishment(searchable);
+
+        // Delete a punishment
+        if (punish.isPresent())
+        {
+            Punishment p = punish.get();
+            p.Delete();
+            sender.sendMessage("History Cleared.");
+            return true;
+        }
+
+        // Delete regex bans
+        if (regexres.isDone())
+        {
+            Optional<ResultSet> ores = regexres.get();
+            if (ores.isPresent())
+            {
+                ResultSet res = ores.get();
+                PreparedStatement RegexDelete = self.connection.prepareStatement("DELETE FROM RegexBans WHERE id = ?");
+
+                while (res.next())
+                {
+                    RegexDelete.setInt(1, res.getInt("id"));
+                    RegexDelete.addBatch();
+                }
+                DatabaseUtil.ExecuteUpdate(RegexDelete);
+                sender.sendMessage("History Cleared.");
+            }
+        }
+
+        // Delete ip bans
+		if (ipbansres.isDone())
+		{
+			Optional<ResultSet> ores = ipbansres.get();
+			if (ores.isPresent())
+            {
+                ResultSet res = ores.get();
+                PreparedStatement IPBanDelete = self.connection.prepareStatement("DELETE FROM IPBans WHERE id = ?");
+
+                while (res.next())
+                {
+                    IPBanDelete.setInt(1, res.getInt("id"));
+                    IPBanDelete.addBatch();
+                }
+                DatabaseUtil.ExecuteUpdate(IPBanDelete);
+                sender.sendMessage("History Cleared.");
+            }
+        }
+        
+        // Delete reports
+        if (reportsres.isDone())
+		{
+			Optional<ResultSet> ores = reportsres.get();
+			if (ores.isPresent())
+            {
+                ResultSet res = ores.get();
+                PreparedStatement ReportDelete = self.connection.prepareStatement("DELETE FROM Reports WHERE id = ?");
+
+                while (res.next())
+                {
+                    ReportDelete.setInt(1, res.getInt("id"));
+                    ReportDelete.addBatch();
+                }
+                DatabaseUtil.ExecuteUpdate(ReportDelete);
+                sender.sendMessage("History Cleared.");
+            }
+        }
+
+        return true;
+	}
+
+    private boolean HandleIPBan(CommandSender sender, String searchable, Timestamp bantime, String[] args) throws SQLException
+    {
+        IPAddressString thingy = new IPAddressString(searchable);
+        for (IPAddressString cb : Main.BannedAddresses) 
+        {
+            if (cb.contains(thingy)) 
+            {
+                PreparedStatement pst = self.connection.prepareStatement("SELECT * FROM IPBans WHERE IPAddress = ? AND TimeAdded >= ?");
+                pst.setString(1, cb.toString());
+                pst.setTimestamp(2, bantime);
+
+                ResultSet res = pst.executeQuery();
+                // Clear the history for this address range
+                if (!res.next())
+                {
+                    sender.sendMessage("The IP " + cb.toString() + " is not banned.");
+                    return true;
+                }
+
+                PreparedStatement ps = self.connection.prepareStatement("DELETE FROM IPBans WHERE IPAddress = ? AND TimeAdded >= ?");
+                ps.setString(1, cb.toString());
+                ps.setTimestamp(2, bantime);
+                
+                DatabaseUtil.ExecuteUpdate(ps);
+                sender.sendMessage("History Cleared.");
+            }
+        }
+
+        return true;
+    }
+
+    private boolean HandlePlayer(CommandSender sender, OfflinePlayer player, Timestamp bantime, String[] args) throws SQLException, ExecutionException, InterruptedException
+    {
+		PreparedStatement ps = self.connection.prepareStatement("SELECT * FROM Punishments WHERE UUID = ? AND TimePunished >= ?");
+        ps.setString(1, player.getUniqueId().toString());
+        ps.setTimestamp(2, bantime);
+
+		Optional<ResultSet> ores = DatabaseUtil.ExecuteLater(ps).get();
+		if (ores.isPresent())
+		{
+			ResultSet res = ores.get();
+			if (res.next())
+			{
+				ps = self.connection.prepareStatement("DELETE FROM Punishments WHERE UUID = ? AND TimePunished >= ?");
+                ps.setString(1, player.getUniqueId().toString());
+				ps.setTimestamp(2, bantime);
+				DatabaseUtil.ExecuteUpdate(ps);
+				sender.sendMessage("History Cleared.");
+			}
+			else
+				sender.sendMessage("Player " + player.getName() + " is not punished.");
+        }
+        return true;
+	}
 
     @Override
     public void onSyntaxError(CommandSender sender, String label, String[] args)
@@ -57,19 +194,26 @@ public class PruneHistoryCommand extends RistExCommandAsync
     {
         if (!PermissionUtil.Check(sender, "lolbans.history.prune"))
             return User.PermissionDenied(sender, "lolbans.history.prune");
-
-        // /prunehistory <Player|PunishID|CIDR|Regex> <Distance In Time|*>
-        if (args.length < 2)
-            return false;
-
+            
         try 
         {
-            String searchable = args[0];
-            String TimePeriod = args[1];
+            // /prunehistory <Player|PunishID|CIDR|Regex> <Distance In Time|*>
+            ArgumentUtil a = new ArgumentUtil(args);
+            a.RequiredString("Searchable", 0);
+            a.RequiredString("Distance", 1);
+
+            if (!a.IsValid())
+                return false;
+
+            String searchable = a.get("Searchable");
+            String TimePeriod = a.get("Distance");
             @SuppressWarnings("deprecation") OfflinePlayer player = Bukkit.getOfflinePlayer(searchable);
             Timestamp bantime = null;
 
             // Parse ban time.
+            // NOTE: We explicitly do this here because it is going back in time instead of forward
+            // which means we cannot use TimeUtil for this. Maybe I will refactor it sometime in the
+            // future but for now this is fine.
             if (!Messages.CompareMany(TimePeriod, new String[]{"*", "0"}))
             {
                 Optional<Long> dur = TimeUtil.Duration(TimePeriod);
@@ -81,73 +225,17 @@ public class PruneHistoryCommand extends RistExCommandAsync
 
             // This only prunes the specific punishment
             if (PunishID.ValidateID(searchable))
-            {
-                // TODO: What about Regex bans or others
-                Optional<Punishment> punish = Punishment.FindPunishment(searchable);
-                if (punish.isPresent())
-                {
-                    Punishment p = punish.get();
-                    p.Delete();
-                }
-
-            }
+                return this.HandlePunishmentID(sender, searchable, args);
             else if (searchable.contains(".") || searchable.contains(":"))
-            {
-                // try
-                // {
-                    IPAddressString thingy = new IPAddressString(searchable);
-                    for (IPAddressString cb : Main.BannedAddresses)
-                    {
-                        if (cb.contains(thingy))
-                        {
-                            PreparedStatement pst = self.connection.prepareStatement("SELECT * FROM IPBans WHERE IPAddress = ?");
-                            pst.setString(1, cb.toString());
-            
-                            ResultSet res = pst.executeQuery();
-                            // Clear the history for this address range
-                            if (!res.next())
-                            {
-                                sender.sendMessage("The IP " + cb.toString() + " is not banned.");
-                                return true;
-                            }
-
-                            PreparedStatement ps = self.connection.prepareStatement("DELETE FROM IPBans WHERE id = ?");
-                            ps.setInt(1, res.getInt("id"));
-                            DatabaseUtil.ExecuteUpdate(ps);
-                            sender.sendMessage("History Cleared.");
-                        }
-                    }
-                // }
-                // catch (AddressStringException e)
-                // {
-                //     sender.sendMessage("Invalid IP Address");
-                // }
-            }
+                return this.HandleIPBan(sender, searchable, bantime, args);
             else if (player != null)
-            {
-                PreparedStatement ps = self.connection.prepareStatement("SELECT * FROM Punishments WHERE UUID = ?");
-                ps.setString(1, player.getUniqueId().toString());
-
-                Optional<ResultSet> ores = DatabaseUtil.ExecuteLater(ps).get();
-                if (ores.isPresent())
-                {
-                    ResultSet res = ores.get();
-                    if (res.next())
-                    {
-                        ps = self.connection.prepareStatement("DELETE FROM Punishments WHERE UUID = ?");
-                        ps.setString(1, player.getUniqueId().toString());
-                        DatabaseUtil.ExecuteUpdate(ps);
-                        sender.sendMessage("History Cleared.");
-                    }
-                    else
-                        sender.sendMessage("Player " + player.getName() + " is not punished.");
-                }
-            }
+                return this.HandlePlayer(sender, player, bantime, args);
             else
             {
                 Pattern regex = Pattern.compile(searchable);
-                PreparedStatement pst = self.connection.prepareStatement("SELECT * FROM RegexBans WHERE Regex = ?");
+                PreparedStatement pst = self.connection.prepareStatement("SELECT * FROM RegexBans WHERE Regex = ? AND TimeAdded >= ?");
                 pst.setString(1, regex.toString());
+                pst.setTimestamp(2, bantime);
 
                 Optional<ResultSet> ores = DatabaseUtil.ExecuteLater(pst).get();
                 if (ores.isPresent())
